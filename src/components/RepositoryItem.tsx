f695e2b8c2f66f3a4c46cbde75859ebe151d9de7
Leandro Guaraldi <leandro_319@yahoo.com.br>
2021-03-10 22:39:59 -0300

    Capitulo 1 - fundamentos React

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,4 +1,12 @@
-export function RepositoryItem (props){
+interface RepositoryItemProps {
+    repository:{
+        name: String,
+        descrition: string,
+        html_url: string;
+    }
+}
+
+export function RepositoryItem (props: RepositoryItemProps){
     return(
         <li>
         <strong>{props.repository.name}</strong>
--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,10 +3,15 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss'
 
+interface Repository {
+  name: string,
+  descrition: string,
+  html_url: string;
+}
 
 export function RepositoryList(){
 
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
     
     useEffect(()=>{
         fetch('https://api.github.com/orgs/rocketseat/repos')
@@ -20,13 +25,11 @@ export function RepositoryList(){
 
             <ul>
               {
-                repositories.map(item=>(
-                  <RepositoryItem repository={item} key={item.name}/>
+                repositories.map(repository=>(
+                  <RepositoryItem repository={repository} key={repository .name}/>
                 )
               )
             }
-
-
             </ul>
         </section>
     )
